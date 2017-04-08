f963c38abc59e581f63d6481bffa156791922212
SiggiSigmann <siggisigmann@googlemail.com>
2017-04-08 20:42:46 +0200

    add Komments

--- TestCase.c
+++ TestCase.c
@@ -7,28 +7,62 @@
 
 #include <stdio.h>
 
+typedef enum{
+	OK, FAIL
+}Test;
+
 typedef struct {
 	unsigned short int Case;
 	unsigned short int Expected;
 	char Name[100];
 } TestCase;
 
+Test testFuntion(int input, int expected);
+
+/*
+ * runTest initialisiert den Testlauf
+ * @param *test Zeiger auf Liste mit Testfällen.
+ * @param Anzahl der Tests.
+ */
 void runTest(int lenght, TestCase *test) {
-	int soultion;
+	Test t;
+	int ok=0;
+	int fail=0;
 	for (int i = 0; i < lenght; i++) {
-		printf("Test Nr.%i (%s): \n", i, test[i].Name);
+		printf("Test Nr.%i (%s): \n", (i+1), test[i].Name);
 		printf("\tInput: %#x; Expected-Output: %#x; ", test[i].Case,
 				test[i].Expected);
-		soultion = test[i].Case;
-		printf("Output: %#x\n", soultion);
-		if (test[i].Expected == soultion) {
+		t = testFuntion(test[i].Case, test[i].Expected);
+		if (OK == t) {
 			printf("\tOK \n");
-		} else
+			ok++;
+		} else{
 			printf("\tFAIL \n");
+			fail++;
+		}
 		printf("\n");
+	}
+	printf("Result Tests: %i OK: %i Fails: %i",lenght,ok,fail);
+}
 
+/*
+ * testFuntion initialisiert den test und liefert das Ergebnis zurück.
+ * @param input Funktionsparameter.
+ * @param expected erwartetes Ergebnis.
+ * @return Ergebis des Tests.
+ */
+Test testFuntion(int input, int expected){
+	int soultion=input;
+
+	printf("Output: %#x\n", soultion);
+	if(soultion==expected){
+		return OK;
+	}else{
+		return FAIL;
 	}
 }
+
+
 
 int main(void) {
 	int number = 3;
